bbb722b8d9dc43b764394c2481f3862d4703e4b3
naikms <125459265+naikms@users.noreply.github.com>
2023-10-12 20:47:35 +0000

    Data Types

---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+3 )  // we are using nodejs, not browser
+
+console.log( 3 
+    +3) // Code readability should be high, We have to see for better readability.
+    // refer Documentation from Mdn and tc39 
+
+    console.log("Mallesh");
+
+
+    let name = "Mallesh"
+    let age  = 26
+    let isLoggedIn = true
+    let state;
+
+
+
+    // number   ==> 2 to power 53
+    // bigint 
+    // string ==> ""
+    // boolean ==> true or false
+    // null ==> standalone value
+    // undefined ==> 
+    // symbol ==> unique
+
+
+
+    // object
+    console.log(typeof "Mallesh");
+    console.log(typeof age);
+    console.log(typeof null);   // type is object
+    console.log(typeof undefined); // type is undefined
