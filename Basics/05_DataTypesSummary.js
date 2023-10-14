25fecc887cec9a8d84d3fa81fb4576b2d1d71e23
naikms <125459265+naikms@users.noreply.github.com>
2023-10-14 06:28:12 +0000

    DataTypes_summary

--- Basics/05_DataTypesSummary.js
+++ Basics/05_DataTypesSummary.js
@@ -14,19 +14,24 @@ const anotherId = Symbol(123)
 
 
 console.log(Id == anotherId);
+console.log(typeof Id);
 
-// const bigNumber = 1248379028031203n
+//const bigNumber = 1248379028031203n
+console.log(typeof bigNumber); // undefined
+console.log(typeof outSideTemp); //object
 
-// reference types or Non-primitive: 
+//******* */ reference types or Non-primitive: ********
 
 // Array,functions, Objects.
 
-const heroe = ["Shaktiman", "Bheem", "Arjun"]; // Arrays has to declare in Square bracket.
+const heros = ["Shaktiman", "Bheem", "Arjun"]; // Arrays has to declare in Square bracket.
 let myobj = {
-    name: "naik", // Objects are in curly backets, 
+    name: "naik", // Objects are in curly backets, in Key: value pair.
     age: 26,
 }
 const myFunction = function(){
     console.log("Helo world");
 }                                    // Here function is stored in variables, In function we can store the any type of data.
- log
+console.log(typeof myFunction); // function
+console.log(typeof heros);      // object
+// https://262.ecma-international.org/5.1/#sec-11.4.3
