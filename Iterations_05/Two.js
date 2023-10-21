62a8012e3bd55ec7a1498b467768ccdcb3a4dcd9
naikms <125459265+naikms@users.noreply.github.com>
2023-10-21 19:54:28 +0000

    "whilw and Do while loops"

---
+++ Iterations_05/Two.js
@@ -0,0 +1,21 @@
+// while
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 3
+}
+
+let myArray = ["Naik", "Manju","Raju"]
+let arr = 0
+while (arr < myArray.length) {
+  //  console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+// Do while
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
